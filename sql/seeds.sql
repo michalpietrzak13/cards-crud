40bc32685b7a89e8fb6570ab16fb6fa4bc8dc17b
Michał <32223466+michalpietrzak13@users.noreply.github.com>
2025-09-30 18:45:42 +0200

    Update seeds.sql

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,4 +1,4 @@
--- SEED RECORDS
+
 INSERT INTO cards (card_number, pin, activated_at, expires_at, balance, created_at, updated_at)
 VALUES
 ('12345678901234567890','1234','2025-09-30 12:00','2027-12-31',100.00,NOW(),NOW()),
